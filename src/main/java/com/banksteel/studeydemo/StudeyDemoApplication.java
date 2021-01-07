23922e6b2103fc0329df874c82c8fdaa8d22c19f
luohuiwen <1217891466@qq.com>
2021-01-07 13:21:04 +0800

    提交到github

---
+++ src/main/java/com/banksteel/studeydemo/StudeyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.banksteel.studeydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudeyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudeyDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/banksteel/studeydemo/StudeyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banksteel.studeydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudeyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
